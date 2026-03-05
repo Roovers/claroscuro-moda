cb502d46a724aa1bb52d4af09e6f62709ae2068e
Juanchi <jcruzroov@gmail.com>
2026-03-05 14:53:59 -0300

    prueba

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,15 +3,14 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyD-sydxNzEBWknmj5ylnKms7R2QePiWV9Y",
+  authDomain: "claroscuro-style-3a9da.firebaseapp.com",
+  projectId: "claroscuro-style-3a9da",
+  storageBucket: "claroscuro-style-3a9da.firebasestorage.app",
+  messagingSenderId: "488308302439",
+  appId: "1:488308302439:web:3165b16b93ee81dacdeac2",
 }
 
 const app = initializeApp(firebaseConfig)
-
 export const db = getFirestore(app)
 export const auth = getAuth(app)
